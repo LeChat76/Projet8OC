cb5cec4e08d9a680636ad101e08f021a6bf1a13f
LeChat76 <cedrik76@msn.com>
2023-11-13 17:17:28 +0100

    test deploiement onrender

--- build.sh
+++ build.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 
-cd /P8
-chmod +x build.sh
-
 # Install dependancies
 /opt/render/project/src/.venv/bin/python3.7 -m pip install --upgrade pip
-/opt/render/project/src/.venv/bin/python3.7 -m pip install -r requirements.txt
 
 # Collect static files
 /opt/render/project/src/.venv/bin/python3.7 manage.py collectstatic --noinput
-
-# launch web service
-gunicorn lechat76_site.wsgi
-
